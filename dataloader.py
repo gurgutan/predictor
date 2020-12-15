1dd157e7ed89b9461663a2bf6472cfc9fdec1d99
Иван Слеповичев <gurgutan@yandex.ru>
2020-12-15 19:15:28 +0400

    имена переменных

--- dataloader.py
+++ dataloader.py
@@ -57,12 +57,14 @@ class Dataloader:
             names=["date", "time", "open", "high", "low", "close", "tickvol", "vol"],
         )
         df_size = df[input_column].size
-        train_len = int(df_size * train_ratio)
-        val_len = int(df_size * val_ratio)
-        test_len = int(df_size * test_ratio)
-        train_slice = slice(-train_len, None)
-        val_slice = slice(-(train_len + val_len), -train_len)
-        test_slice = slice(-(train_len + val_len + test_len), -(train_len + val_len))
+        train_size = int(df_size * train_ratio)
+        val_size = int(df_size * val_ratio)
+        test_size = int(df_size * test_ratio)
+        train_slice = slice(-train_size, None)
+        val_slice = slice(-(train_size + val_size), -train_size)
+        test_slice = slice(
+            -(train_size + val_size + test_size), -(train_size + val_size)
+        )
         self.train_df = df[input_column][train_slice]
         self.val_df = df[input_column][val_slice]
         self.test_df = df[input_column][test_slice]
